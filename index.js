0d06fda5dafb9b6caaf9a844a1e4c54978624234
JamieM13 <112992988+JamieM13@users.noreply.github.com>
2022-11-20 21:39:46 -0600

    Added join room and 2 player limit
    I added a "join dissonance" button, which disappears when there are more than 2 people in a room. The mousePos is only tracked and sent for the first 2 people.

--- index.js
+++ index.js
@@ -36,78 +36,98 @@ server.listen(port, () => {
 let io = require('socket.io');
 io = new io.Server(server);
 
-let users = [];
+let disPlayers = [];
 let currentUser;
 let userGone;
 let distBetween;
+let disCount=0;
+let conCount=0;
 
 
 //Listen for individual clients/users to connect
 io.sockets.on('connection', function (socket) {
     console.log("We have a new client: " + socket.id);
-    //on connection from a new browser, add socket.id to an array, initialize it at X=0 y=0
-    users.push({
-        x: 0,
-        y: 0,
-        id: socket.id,
-        xDist: 0,
-        yDist: 0,
-        totalDist: 0
-    });
-    console.log(users[0]);
+    io.sockets.emit('disCount', disCount);
+
+
     //when a user joins, send that user its own socket ID
     socket.emit('userID', socket.id);
 
+    socket.on('joinRoom', function (room){
+        console.log("join request received");
+        if (room == "dis"){
+            console.log(disCount);
+            if (disCount<2) {
+                disPlayers.push({
+                    x: 0,
+                    y: 0,
+                    id: socket.id,
+                    xDist: 0,
+                    yDist: 0,
+                    totalDist: 0
+                });
+                disCount++;
+                console.log(disPlayers[0]);
+                //when a user joins, send that user its own socket ID
+                socket.emit('playerStatus', "Playing");
+                
+            } else {
+                socket.emit('playerStatus', "Observing");
+            }
+            io.sockets.emit('disCount', disCount);
+        }
+    });
+
     //Listen for a message named 'data' from this client
-    socket.on('data', function (data) {
+    socket.on('dataDis', function (data) {
         //Data can be numbers, strings, objects
         console.log("Received: 'data' " + data);
 
         //assign incoming user data to an index in our users array so we can distinguish between users.
         //check incoming data.socket.id against the socket ids of the users that have connected
         //identify which user's data is associated with the data that has come in
-        for (i = 0; i < users.length; i++) {
-            if (users[i].id == data.id) {
+        for (i = 0; i < disPlayers.length; i++) {
+            if (disPlayers[i].id == data.id) {
                 currentUser = i;
             }
         }
 
 
         //grab incoming data of mouseX and mouseY and update users array with current XY data
-        users[currentUser].x = data.x;
-        users[currentUser].y = data.y;
+        disPlayers[currentUser].x = data.x;
+        disPlayers[currentUser].y = data.y;
 
         //-----------calculate distances ---------
         let xTotal = 0;
         let yTotal = 0;
 
-        // establish an average point between the xy coordinates of the users
-        for (i = 0; i < users.length; i++) {
-            xTotal = users[i].x + xTotal;
-            yTotal = users[i].y + yTotal;
+        // establish an average point between the xy coordinates of the disPlayers
+        for (i = 0; i < disPlayers.length; i++) {
+            xTotal = disPlayers[i].x + xTotal;
+            yTotal = disPlayers[i].y + yTotal;
         }
 
-        let avgX = xTotal / users.length;
-        let avgY = yTotal / users.length;
+        let avgX = xTotal / disPlayers.length;
+        let avgY = yTotal / disPlayers.length;
 
         // calculate the distance between the user and the average point
 
-        for (i = 0; i < users.length; i++) {
-            users[i].xDist = users[i].x - avgX;
-            users[i].yDist = users[i].y - avgY;
-            let x = users[i].xDist;
-            let y = users[i].yDist;
+        for (i = 0; i < disPlayers.length; i++) {
+            disPlayers[i].xDist = disPlayers[i].x - avgX;
+            disPlayers[i].yDist = disPlayers[i].y - avgY;
+            let x = disPlayers[i].xDist;
+            let y = disPlayers[i].yDist;
             distBetween = Math.sqrt(x * x + y * y);
-            users[i].totalDist = distBetween;
+            disPlayers[i].totalDist = distBetween;
         }
 
-        console.log("distance =" + distBetween);
+        // console.log("distance =" + distBetween);
 
 
 
         //Send the data to all clients, including this one
         //Set the name of the message to be 'data'
-        io.sockets.emit('data', users);
+        io.sockets.emit('data', disPlayers);
 
         //Send the data to all other clients, not including this one
         // socket.broadcast.emit('data', data);
@@ -120,13 +140,14 @@ io.sockets.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log("A client has disconnected: " + socket.id);
 
-        for (i = 0; i < users.length; i++) {
-            if (users[i].id == socket.id) {
+        for (i = 0; i < disPlayers.length; i++) {
+            if (disPlayers[i].id == socket.id) {
                 userGone = i;
+                disCount--;
             }
         }
-        users.splice(userGone, 1);
+        disPlayers.splice(userGone, 1);
         console.log("this guy left" + userGone);
-        console.log("these are here" + users);
+        console.log("these are here" + disPlayers);
     });
 });
--- public/javascripts/dissonance_app.js
+++ public/javascripts/dissonance_app.js
@@ -2,17 +2,43 @@
 let socket = io();
 let myId;
 let myPos;
+let myStatus;
 
 //Listen for confirmation of connection
 socket.on('connect', function () {
   console.log("Connected");
 });
 
+
+//when button is clicked, emit message to server that allows for passing mouseXY positions back and forth
+let joinDis = document.getElementById('joinDis');
+joinDis.addEventListener("click", joinRoom);
+
+
 // listen for what your socket ID is so you know who you are in the object
 socket.on('userID', function (socketId) {
   console.log("I am " + socketId);
   myId = socketId;
 });
+
+// listen for what your socket ID is so you know who you are in the object
+socket.on('disCount', function (disCount) {
+  if (disCount<2) {
+    joinDis.style.visibility = "visible";
+  } else {
+    joinDis.style.visibility = "hidden";
+  }
+});
+
+// listen for what your socket ID is so you know who you are in the object
+socket.on('playerStatus', function (playerStatus) {
+  myStatus = playerStatus;
+});
+
+
+function joinRoom() {
+  socket.emit('joinRoom', "dis");
+}
 
 
 // ------------------This is where you can find variables to play with! myPos contains the distance of x and y from an average of all users' x and y. Theoretically this should mean that it works for as many users as we want to have join. myPos is an object that has the x position, y position, distance from average for x, distance from average for y, and the total distance from the average point. It is console logged in the browser. These elements for the myPos object are defined in the users array in index.js
@@ -117,10 +143,13 @@ function setup() {
 
 
 function mouseMoved() {
-  //Grab mouse position
-  let mousePos = { x: mouseX, y: mouseY, id: socket.id };
-  //Send mouse position object to the server
-  socket.emit('data', mousePos);
+  if (myStatus == "Playing"){
+      //Grab mouse position
+    let mousePos = { x: mouseX, y: mouseY, id: socket.id };
+    //Send mouse position object to the server
+    socket.emit('dataDis', mousePos);
+  }
+
 
   // Draw yourself at your xy position
   // ----- this is where you can put something different at the cursor.
